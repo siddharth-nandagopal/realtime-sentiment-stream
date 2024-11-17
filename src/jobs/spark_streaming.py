7667acb105cc15b3c8c6d0048eda62c501b96053
Siddharth Nandagopal <sid.nandhan@gmail.com>
2024-11-17 11:17:49 -0500

    extract the data from the incoming stream message and display in the console

--- src/jobs/spark_streaming.py
+++ src/jobs/spark_streaming.py
@@ -1,7 +1,8 @@
 from time import sleep
 
 from pyspark.sql import SparkSession
-
+from pyspark.sql.functions import from_json, col
+from pyspark.sql.types import StructType, StructField, StringType, FloatType
 
 
 def start_streaming(spark):
@@ -11,9 +12,26 @@ def start_streaming(spark):
                         .option("host", "localhost")
                         .option("port", 9999)
                         .load()
-                        )
+                    )
+        
+        schema = StructType([
+                StructField("review_id", StringType()),
+                StructField("user_id", StringType()),
+                StructField("business_id", StringType()),
+                StructField("stars", FloatType()),
+                StructField("date", StringType()),
+                StructField("text", StringType())
+            ])
 
-        query = stream_df.writeStream.outputMode("append").format('console').start()
+        # when the data comes in thru the stream, it has the JSON data under the column 'value'
+        # get the JSON data from 'value' column and map it to the schema
+        # put the JSON data in the alias 'data' (similar to SQL)
+        # select all the records from the alias 'data'
+        stream_df = stream_df.select(from_json(col('value'), schema)
+                                    .alias("data")
+                                    ).select(("data.*"))
+        
+        query = stream_df.writeStream.outputMode("append").format("console").options(truncate=False).start()
         query.awaitTermination()
 
 
